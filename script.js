91cfdcf19af5b95df69f99841e5a3ebbfa172e6a
Jesper Kjærgaard <jezper@hotmail.dk>
2023-11-02 19:54:39 +0100

    feat -  js check email is valid

---
+++ script.js
@@ -0,0 +1,17 @@
+const errorLabel = document.querySelector('.label-error')
+const validRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+function SubmitEmail(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById('email')
+  
+  if (!email.value.match(validRegex)) {
+    email.className = 'email-error'
+    errorLabel.style.display = 'block';    
+    return
+  }
+
+  window.location.href = '/success.html';
+}
